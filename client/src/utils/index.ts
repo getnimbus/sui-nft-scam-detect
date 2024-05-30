cb8845a4f7289ffa22d92daa8eacd7cb85f55bbc
huong666 <nguyenxuananhuong541@gmail.com>
2024-05-30 14:36:50 +0700

    add classify to fe site

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,5 +1,11 @@
 import numeral from "numeral";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export const formatCurrency = (input: number) => {
   return numeral(input).format("0,0.[000000]");
 };
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}

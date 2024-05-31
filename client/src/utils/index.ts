0c9eabc0aad2f1a8c6d0895ce3c533d2625db47a
huong666 <nguyenxuananhuong541@gmail.com>
2024-05-31 17:45:01 +0700

    update formatCurrency

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -3,7 +3,9 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const formatCurrency = (input: number) => {
-  return numeral(input).format("0,0.[000000]");
+  return numeral(input).format("0.00000") === "NaN"
+    ? 0.000001
+    : numeral(input).format("0,0.[00000]");
 };
 
 export function cn(...inputs: ClassValue[]) {
